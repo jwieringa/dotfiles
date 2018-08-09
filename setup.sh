2b2128e211de109f9df9557355fdfadfd328e599
Jason Wieringa <jason@wieringa.io>
2018-08-09 09:04:01 -0700

    Update Mac setup script

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
-ln -s vimrc ~/.vimrc
-ln -s install-ruby-installer /opt/bin/install-ruby-installer
-ln -s install-a-ruby /opt/bin/install-a-ruby
-ln -s hashicorp-install /opt/bin/hashicorp-install
+brew install zlib pyenv gpg readline xz
+
+# ln -s vimrc ~/.vimrc
+ln -s ~/prj/deps/dotfiles/install-ruby-installer /opt/bin/install-ruby-installer
+ln -s ~/prj/deps/dotfiles/install-a-ruby /opt/bin/install-a-ruby
+ln -s ~/prj/deps/dotfiles/hashicorp-install /opt/bin/hashicorp-install
